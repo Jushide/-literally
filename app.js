7edb330af3d16302fb69a66dd92caf6feb5f5454
Jushide <maciuxn@gmail.com>
2023-04-15 13:17:53 +0200

    Working app with words in array

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 "use strict"
 
-const words = ["siara", "krowa"]
-const current_word = words[1].split("")
+const randomizedWordIdx = Math.floor(Math.random() * 2)
+const words = ["siara", "KroWa"]
+const current_word = words[randomizedWordIdx].toLowerCase().split("")
 let boxesNumber = 1
+let tries = 1
+let currentPoints = 0
 let rowsChildrens = {}
+let correctLetters = Array(5)
 
 for (let i = 1; i <= 6; i++) {
     let rowsNum = `row${i}`
@@ -15,37 +19,45 @@ function checkWord() {
         let user_word = []
 
         for (let idx = 0; idx < 1; idx++) {
-            // console.log(rowsChildrens[`row${boxesNumber}`][idx])
             if (rowsChildrens[`row${boxesNumber}`][idx].children[0].hasAttribute("disabled") === false) {
                 let user_letter
+
                 for (let x = 0; x <= 4; x++) {
                     if (user_word.length < current_word.length) {
-                        user_letter = rowsChildrens[`row${boxesNumber}`][x].children[0].value
+                        user_letter = rowsChildrens[`row${boxesNumber}`][x].children[0].value.toLowerCase()
                         user_word.push(user_letter)
                         rowsChildrens[`row${boxesNumber}`][x].children[0].setAttribute("disabled", "disabled")
-                        if (current_word.includes(user_letter)) {
-                            const letter_index_in_current_word = current_word.indexOf(user_letter)
-                            const letter_index_in_given_word = user_word.indexOf(user_letter)
-                            if (letter_index_in_current_word === letter_index_in_given_word) {
-                                rowsChildrens[`row${boxesNumber}`][x].children[0].style.backgroundColor = "rgb(116, 173, 31)"
-                            } else
-                                rowsChildrens[`row${boxesNumber}`][x].children[0].style.backgroundColor = "rgb(145, 145, 13)"
-                        } else rowsChildrens[`row${boxesNumber}`][x].children[0].style.backgroundColor = "grey"
+
+                        if (user_letter === current_word[x]) {
+                            rowsChildrens[`row${boxesNumber}`][x].children[0].style.backgroundColor = "rgb(116, 173, 31)"
+                            if (user_letter != correctLetters[x] && current_word[x] === user_letter) {
+                                currentPoints++
+                                correctLetters[x] = user_letter
+                            }
+                        } else if (current_word.includes(user_letter))
+                            rowsChildrens[`row${boxesNumber}`][x].children[0].style.backgroundColor = "rgb(145, 145, 13)"
+                        else rowsChildrens[`row${boxesNumber}`][x].children[0].style.backgroundColor = "grey"
                     }
                 }
             }
         }
-        console.log(user_word, current_word, boxesNumber)
 
-        user_word = []
-
-        if (boxesNumber < 6)
+        if (boxesNumber < 6 && currentPoints < 5)
             for (let idx = 0; idx < 1; idx++) {
                 if (rowsChildrens[`row${boxesNumber + 1}`][idx].children[0].hasAttribute("disabled") === true)
                     for (let x = 0; x <= 4; x++)
                         rowsChildrens[`row${boxesNumber + 1}`][x].children[0].removeAttribute("disabled")
             }
+
+        if (currentPoints === current_word.length && tries < 7) {
+            document.getElementById("win").innerHTML += ` ${tries} tries`
+            document.getElementById("win").style.setProperty("display", "block")
+        } else if (currentPoints < current_word.length && tries === 6) {
+            document.getElementById("lose").style.setProperty("display", "block")
+        } else tries++
+
         ++boxesNumber
+        user_word = []
     }
 }
 
