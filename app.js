b4c3b0b49cdb3ae3892d99f5e93cedbc4413fc0f
Jushide <maciuxn@gmail.com>
2023-04-15 11:26:21 +0200

    Changing to next boxes, checking if letter is correct and depends on place changing color

--- app.js
+++ app.js
@@ -1,56 +1,58 @@
+"use strict"
+
 const words = ["siara", "krowa"]
-const current_word = words[0].split("")
-let boxesNumber = 0
+const current_word = words[1].split("")
+let boxesNumber = 1
 let rowsChildrens = {}
 
 for (let i = 1; i <= 6; i++) {
-    rowsNum = `row${i}`
+    let rowsNum = `row${i}`
     rowsChildrens[rowsNum] = document.querySelector(`.${rowsNum}`).children
 }
-console.log(rowsChildrens)
 
-document.getElementById("submit_word").addEventListener("click", () => {
-    ++boxesNumber
-    let user_word = []
+function checkWord() {
+    if (boxesNumber < 7) {
+        let user_word = []
 
-    console.log(rowsChildrens["row1"][0].children[0].hasAttribute("disabled"))
-    for (let i in rowsChildrens.length) {
-        if (rowsChildrens[`row1`][i].children[0].hasAttribute("disabled") === false)
-            for (let x = 0; x <= 4; x++) {
-                user_word.push(rowsChildrens["row1"][x])
+        for (let idx = 0; idx < 1; idx++) {
+            // console.log(rowsChildrens[`row${boxesNumber}`][idx])
+            if (rowsChildrens[`row${boxesNumber}`][idx].children[0].hasAttribute("disabled") === false) {
+                let user_letter
+                for (let x = 0; x <= 4; x++) {
+                    if (user_word.length < current_word.length) {
+                        user_letter = rowsChildrens[`row${boxesNumber}`][x].children[0].value
+                        user_word.push(user_letter)
+                        rowsChildrens[`row${boxesNumber}`][x].children[0].setAttribute("disabled", "disabled")
+                        if (current_word.includes(user_letter)) {
+                            const letter_index_in_current_word = current_word.indexOf(user_letter)
+                            const letter_index_in_given_word = user_word.indexOf(user_letter)
+                            if (letter_index_in_current_word === letter_index_in_given_word) {
+                                rowsChildrens[`row${boxesNumber}`][x].children[0].style.backgroundColor = "rgb(116, 173, 31)"
+                            } else
+                                rowsChildrens[`row${boxesNumber}`][x].children[0].style.backgroundColor = "rgb(145, 145, 13)"
+                        } else rowsChildrens[`row${boxesNumber}`][x].children[0].style.backgroundColor = "grey"
+                    }
+                }
             }
+        }
+        console.log(user_word, current_word, boxesNumber)
+
+        user_word = []
+
+        if (boxesNumber < 6)
+            for (let idx = 0; idx < 1; idx++) {
+                if (rowsChildrens[`row${boxesNumber + 1}`][idx].children[0].hasAttribute("disabled") === true)
+                    for (let x = 0; x <= 4; x++)
+                        rowsChildrens[`row${boxesNumber + 1}`][x].children[0].removeAttribute("disabled")
+            }
+        ++boxesNumber
     }
-    console.log(user_word, current_word, boxesNumber)
+}
 
-    // const user_word = document.getElementById("input_word").value.split("")
-    // * 2 sposob
-    // ++boxesNumber
-    // let user_word = []
-    // console.log(rowsChildrens["row1"][0].children[0].hasAttribute("disabled"))
-    // for (let i in rowsChildrens.length) {
-    //     if (rowsChildrens[`row1`][i].children[0].hasAttribute("disabled") === false)
-    //         for (let x = 0; x <= 4; x++) {
-    //             user_word.push(rowsChildrens["row1"][x])
-    //         }
-    // }
-    // console.log(user_word, current_word, boxesNumber)
-
-    // * 1 sposob
-    // for (const [index, user_letter] of user_word.entries()) {
-    //     if (current_word.includes(user_letter)) {
-    //         const letter_index = current_word.indexOf(user_letter)
-    //         console.log(user_letter, letter_index, boxesNumber)
-    //         if (index === letter_index) {
-    //             document.querySelector(`.box1${letter_index + 1}`).classList.add("correct")
-    //         } else {
-    //             document.querySelector(`.box1${letter_index + 1}`).classList.add("wrong-place")
-    //             document.querySelector(`.box1${letter_index + 1}`).innerHTML = user_letter
-    //         }
-    //     } else {
-    //         console.log(null)
-    //     }
-    // }
-    user_word = []
+document.body.addEventListener("keyup", e => {
+    if (e.keyCode === 13) {
+        checkWord()
+    }
 })
 
-// let duplicate = current_word => current_word.filter((item, index) => current_word.indexOf(item) != index)
+document.getElementById("submit_word").addEventListener("click", checkWord)
